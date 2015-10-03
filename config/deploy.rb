16d4d6d27a2c8fac38b56820dd2d04917eee028f
sunita4live <sunita4live@rediffmail.com>
2015-10-03 16:48:03 -0400

    unicorn setting changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,14 +54,14 @@ namespace :deploy do
     end
   end
   
-  #after :restart, :clear_cache do
-  #  on roles(:web), in: :groups, limit: 3, wait: 10 do
-  #    # Here we can do anything such as:
-  #    # within release_path do
-  #    #   execute :rake, 'cache:clear'
-  #    # end
-  #  end
-  #end
+  after :restart, :clear_cache do
+   on roles(:web), in: :groups, limit: 3, wait: 10 do
+     # Here we can do anything such as:
+     # within release_path do
+     #   execute :rake, 'cache:clear'
+     # end
+   end
+  end
   
   after 'deploy:publishing', 'deploy:restart'  
   task :restart, :clear_cache do
